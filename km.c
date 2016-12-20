16d58e2e0db5a63cb89b185c4db824b82772dbc8
byllgrim <robin.a.t.pedersen@gmail.com>
2016-12-20 13:18:10 +0000

    initialize rows and cols

--- km.c
+++ km.c
@@ -1,6 +1,9 @@
 #include <bcm2835.h>
 #include <stdio.h>
 
+/* macros and defines */
+#define LEN(X) (sizeof X / sizeof X[0])
+
 #define C0 RPI_V2_GPIO_P1_03
 #define C1 RPI_V2_GPIO_P1_05
 #define C2 RPI_V2_GPIO_P1_07
@@ -19,11 +22,33 @@
 #define R2 RPI_V2_GPIO_P1_24
 #define R0 RPI_V2_GPIO_P1_26
 
+/* global variables */
+static uint8_t cols[8] = {C0, C1, C2, C3, C4, C5, C6, C7};
+static uint8_t rows[8] = {R0, R1, R2, R3, R4, R5, R6, R7};
+
+/* function definitions */
+void
+init()
+{
+	uint8_t i;
+
+	for (i = 0; i < LEN(cols); i++) {
+		bcm2835_gpio_fsel(cols[i], BCM2835_GPIO_FSEL_OUTP);
+		bcm2835_gpio_write(cols[i], LOW);
+	}
+
+	for (i = 0; i < LEN(rows); i++) {
+		bcm2835_gpio_fsel(rows[i], BCM2835_GPIO_FSEL_INPT);
+		bcm2835_gpio_set_pud(rows[i], BCM2835_GPIO_PUD_DOWN);
+	}
+}
+
 int
 main(int argc, char *argv[])
 {
 	if (!bcm2835_init())
 		return 1;
+	init();
 
 	bcm2835_close();
 	return 0;
