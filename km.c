804ac9472a6f671ac5e1b779208c2b9fab39672b
byllgrim <robin.a.t.pedersen@gmail.com>
2016-12-20 16:22:53 +0000

    detect pressed keys

--- km.c
+++ km.c
@@ -4,6 +4,9 @@
 /* macros and defines */
 #define LEN(X) (sizeof X / sizeof X[0])
 
+#define ROWNUM 8
+#define COLNUM 8
+
 #define C0 RPI_V2_GPIO_P1_03
 #define C1 RPI_V2_GPIO_P1_05
 #define C2 RPI_V2_GPIO_P1_07
@@ -22,13 +25,20 @@
 #define R2 RPI_V2_GPIO_P1_24
 #define R0 RPI_V2_GPIO_P1_26
 
+/* function declarations */
+static void init(void);
+static void updatematrix(void);
+static void checkrows(uint8_t col);
+static void printmatrix(void);
+
 /* global variables */
-static uint8_t cols[8] = {C0, C1, C2, C3, C4, C5, C6, C7};
-static uint8_t rows[8] = {R0, R1, R2, R3, R4, R5, R6, R7};
+static uint8_t cols[ROWNUM] = {C0, C1, C2, C3, C4, C5, C6, C7};
+static uint8_t rows[COLNUM] = {R0, R1, R2, R3, R4, R5, R6, R7};
+static uint8_t matrix[4][14] = {0}; /* TODO fix number macros */
 
 /* function definitions */
 void
-init()
+init(void)
 {
 	uint8_t i;
 
@@ -43,12 +53,64 @@ init()
 	}
 }
 
+void
+updatematrix(void)
+{
+	uint8_t m, n;
+
+	for (n = 0; n < LEN(cols); n++) {
+		bcm2835_gpio_write(cols[n], HIGH);
+		checkrows(n);
+		bcm2835_gpio_write(cols[n], LOW);
+	}
+}
+
+void
+checkrows(uint8_t col)
+{
+	uint8_t i, v, c, r;
+
+	/* TODO merge into 1 for loop if possible */
+	for (i = 0; i < LEN(rows); i += 2) { /* red */
+		v = bcm2835_gpio_lev(rows[i]);
+		r = i/2;
+		c = col*2 - 1;
+		if (col != 0)
+			matrix[r][c] = !!v; /* TODO use real value? */
+	}
+
+	for (i = 1; i < LEN(rows); i += 2) { /* blue */
+		v = bcm2835_gpio_lev(rows[i]);
+		r = (i - 1)/2;
+		c = col*2;
+		if (col != 7)
+			matrix[r][c] = !!v;
+	}
+}
+
+void
+printmatrix(void)
+{
+	uint8_t m, n;
+
+	for (m = 0; m < 4; m++) {
+		for (n = 0; n < 14; n++) {
+			matrix[m][n] ? printf("■")
+			             : printf("□");
+		}
+		printf("\n");
+	}
+}
+
 int
 main(int argc, char *argv[])
 {
 	if (!bcm2835_init())
 		return 1;
 	init();
+
+	updatematrix();
+	printmatrix();
 
 	bcm2835_close();
 	return 0;
